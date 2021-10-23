5facc7b3d4da060794e1ba8d5352457a3789ce36
bimoimans <bimoimansmartadi@gmail.com>
2021-10-23 16:34:44 +0700

    Running chat room app is now working. But not properly yet

--- chat_room/chat_room/settings.py
+++ chat_room/chat_room/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'chat_room_app',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'notifications',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# notifications settings 
+NOTIFICATIONS_CHANNELS = {
+   'websocket': 'chat.channels.BroadCastWebSocketChannel'
+}
