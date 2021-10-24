e2bae400de15477ae14939ffe0d432ae165a164c
bimoimans <bimoimansmartadi@gmail.com>
2021-10-24 09:36:11 +0700

    Added file models

--- file_service/file_service_app/models.py
+++ file_service/file_service_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UploadedFile(models.Model):
+    uploaded_file = models.FileField(upload_to='uploaded_file/')
+
+class CompressedFile(models.Model):
+    compressed_file = models.FileField(upload_to='compressed_file/')
