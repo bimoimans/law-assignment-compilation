1af8a9a77a281e4da45008feead0e76e9eee835e
bimoimans <bimoimansmartadi@gmail.com>
2021-10-25 20:16:43 +0700

    Deleted signup html, edited urls and views

--- chat_room/chat_signin/urls.py
+++ chat_room/chat_signin/urls.py
@@ -8,5 +8,6 @@ from . import views
 urlpatterns = [
     path('signin/', views.open_signin_page),
     path('signup/', views.open_signup_page),
-    path('cek/', views.cek_request)
+    path('cek/', views.print_session),
+    path('handle_signin/', views.sign_in_handler)
 ]
--- chat_room/chat_signin/views.py
+++ chat_room/chat_signin/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render, resolve_url
+from django.http import  HttpResponse, HttpResponseRedirect
 
+import requests
+
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def open_signin_page(request):
-    return render(request, "signin.html", {'endpoint': 'http://localhost:8000/auth/token/login/?format=json', 'button_text':"sign in"})
+    return render(request, "signin.html", {'endpoint': 'http://localhost:8000/chat_room/handle_signin/', 'button_text':"sign in"})
 
 def open_signup_page(request):
     return render(request, "signin.html", {'endpoint': 'http://localhost:8000/auth/users/?format=json', 'button_text':"sign up"})
 
-def cek_request(request):
-    print(request)
-    return render(request, "signin.html", {'endpoint': '/auth/token/login/', 'button_text':"sign in"})
+@csrf_exempt
+def sign_in_handler(request):
+    if request.method == "POST":
+        url = "http://localhost:8000/auth/token/login/?format=json"
+        username = request.POST["username"]
+        password = request.POST["password"]
+        payload = {"username": username,
+                   "password": password}
+        requested_token = requests.post(url, data=payload).json()
+        request.session["access_token"] = requested_token["auth_token"]
+    return HttpResponseRedirect('http://localhost:8000/chat_room/chats/list/')
 
+def print_session(request):
+    print(request.session.get("access_token",0))
+    return HttpResponseRedirect('http://localhost:8000/chat_room/chats/list')
+
+def sign_up_handler(request):
+    return
 # def signin(request):
 #     return request
 
