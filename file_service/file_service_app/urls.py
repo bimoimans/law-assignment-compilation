5310f9405c0e12cb3095d5d522451a4eb6739d6b
bimoimans <bimoimansmartadi@gmail.com>
2021-10-26 22:52:28 +0700

    Added url for download media file

--- file_service/file_service_app/urls.py
+++ file_service/file_service_app/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.urls import path 
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
   
 urlpatterns = [
     path('compress/', views.compress_file),
     path('delall/', views.del_all)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
